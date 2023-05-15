b8c0db4924227b03ecbcb479b775e039a246e72b
Mangoccino <mickey.chen19@gmail.com>
2023-05-15 14:38:02 +0930

    Added javascript to html file

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-function getComputerChoice()
-{
-    return Math.floor(Math.random()*3);
-}
+//Date: 15/05/23
+
+//Declaring variables
+const options = ['rock', 'paper', 'scissors'];
+
+const rockButton = document.getElementById("rock");
+rockButton.addEventListener('click', function() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(options[randomNumber]);
+});
